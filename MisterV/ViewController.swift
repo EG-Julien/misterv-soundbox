613f6140d9d6b37428b1132c4f3b59f4ab5bcb8d
Ju <julien.geneste@free.fr>
2021-04-21 18:59:12 +0200

    MisterV SoundBox 4 sounds

--- MisterV/ViewController.swift
+++ MisterV/ViewController.swift
@@ -6,14 +6,76 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var audioPlayer:AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func aTapped(_ sender: Any) {
+        
+        let url = Bundle.main.url(forResource: "encore", withExtension: "mp3")
+        
+        guard url != nil else {
+            return
+        }
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: url!)
+            audioPlayer?.play()
+        } catch {
+            print("error")
+        }
+    }
+    
+    @IBAction func bTapped(_ sender: Any) {
+        let url = Bundle.main.url(forResource: "cfbln", withExtension: "mp3")
+        
+        guard url != nil else {
+            return
+        }
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: url!)
+            audioPlayer?.play()
+        } catch {
+            print("error")
+        }
+    }
+    
+    @IBAction func cTapped(_ sender: Any) {
+        let url = Bundle.main.url(forResource: "chomage", withExtension: "mp3")
+        
+        guard url != nil else {
+            return
+        }
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: url!)
+            audioPlayer?.play()
+        } catch {
+            print("error")
+        }
+    }
+    
+    @IBAction func dTapped(_ sender: Any) {
+        let url = Bundle.main.url(forResource: "thug", withExtension: "mp3")
+        
+        guard url != nil else {
+            return
+        }
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: url!)
+            audioPlayer?.play()
+        } catch {
+            print("error")
+        }
+    }
 }
 
